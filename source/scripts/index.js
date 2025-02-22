8c6d14204d4a60b1349b1f44bfdbd26a15db0915
Konstantin-CPU <konstantingoncharuk50@gmail.com>
2025-02-22 17:56:44 +0200

    Завершение главной страницы

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,9 @@
 /* в этот файл добавляет скрипты*/
+let toggleButton = document.querySelector('.header__toggle');
+let headerNavigation = document.querySelector('.navigation');
+
+toggleButton.addEventListener('click', (evt) => {
+  evt.preventDefault;
+  toggleButton.classList.toggle('header__toggle--opened');
+  headerNavigation.classList.toggle('navigation--opened');
+});
